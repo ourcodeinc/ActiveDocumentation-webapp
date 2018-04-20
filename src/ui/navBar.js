4d44c564cf9ba6c0c485f6ffe506dfc4bf466815
Sahar Mehrpour <saharmehrpour@Sahars-MBP.vsnet.gmu.edu>
2018-04-20 16:43:54 -0400

    navBar is rewritten in react-Bootstrap

--- src/ui/navBar.js
+++ src/ui/navBar.js
@@ -7,6 +7,7 @@ import React, {Component} from 'react';
 import '../App.css';
 import FaArrowLeft from 'react-icons/lib/fa/arrow-left';
 import FaArrowRight from 'react-icons/lib/fa/arrow-right';
+import {Nav, Navbar, NavItem} from "react-bootstrap";
 
 // import ReactDOM from 'react-dom';
 
@@ -14,21 +15,34 @@ export class NavBar extends Component {
 
     render() {
         return (
-            <nav className="navbar navbar-inverse navbar-fixed-top">
-                <div className="container-fluid">
-                    <ul className="nav navbar-nav">
-                        <li className="disabled"><a id="back_button"><FaArrowLeft size={25}/></a></li>
-                        <li className="disabled"><a id="forward_button"><FaArrowRight size={25}/></a></li>
-                    </ul>
-                    <ul className="nav navbar-nav navbar-right">
-                        <li><a href="#/index">Table of Content</a></li>
-                        <li><a href="#/rules">Rules</a></li>
-                        {/*<li className="disabled"><a>Project Hierarchy</a></li>*/}
-                        {/*href="#/hierarchy"*/}
-                        <li><a href="#/genRule">Generate Rules</a></li>
-                    </ul>
-                </div>
-            </nav>
+            <Navbar inverse collapseOnSelect
+                    style={{backgroundColor: "transparent", backgroundImage: "none", border: "none"}}>
+                <Navbar.Header>
+                    <Nav>
+                        <NavItem eventKey={1} className="disabled" id="back_button">
+                            <FaArrowLeft size={20}/>
+                        </NavItem>
+                        <NavItem eventKey={2} className="disabled" id="forward_button">
+                            <FaArrowRight size={20}/>
+                        </NavItem>
+                    </Nav>
+                    <Navbar.Toggle/>
+                </Navbar.Header>
+                <Navbar.Collapse>
+                    <Nav
+                        onSelect={key => {window.location.hash = (key === 1) ? "#/index" : (key === 2) ? "#/rules" : (key === 3) ? "#/genRule" : "#/index"}}>
+                        <NavItem eventKey={1}>
+                            Table of Content
+                        </NavItem>
+                        <NavItem eventKey={2}>
+                            Rules
+                        </NavItem>
+                        <NavItem eventKey={3}>
+                            Generate Rules
+                        </NavItem>
+                    </Nav>
+                </Navbar.Collapse>
+            </Navbar>
         )
     }
 
