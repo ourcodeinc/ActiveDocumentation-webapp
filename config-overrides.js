5fab977367371b78abd886d302ff03adc657a9d3
saharmehrpour <smehrpou@masonlive.gmu.edu>
2021-05-17 06:20:53 -0400

    prepared files for build

---
+++ config-overrides.js
@@ -0,0 +1,26 @@
+const MonacoWebpackPlugin = require('monaco-editor-webpack-plugin');
+
+module.exports = (config /*, env*/) => {
+
+    // console.log(config);
+
+    // Heavy bundle! There are options though
+    config.optimization.minimize = false;
+
+    // Monaco ESM start
+    const options = {
+        languages: ['css', 'handlebars', 'html', 'javascript', 'typescript',
+            'json', 'less', 'scss', 'xml'],
+        features: ['accessibilityHelp', 'bracketMatching', 'caretOperations', 'clipboard',
+            'codeAction', 'codelens', 'colorDetector', 'comment', 'contextmenu',
+            'coreCommands', 'cursorUndo', 'dnd', 'find', 'folding', 'fontZoom',
+            'format', 'gotoError', 'gotoLine', 'gotoSymbol', 'hover', '!iPadShowKeyboard',
+            'inPlaceReplace', '!inspectTokens', 'linesOperations', 'links', 'multicursor',
+            'parameterHints', 'quickCommand', 'quickOutline', 'referenceSearch', 'rename',
+            'smartSelect', 'snippets', 'suggest', '!toggleHighContrast', '!toggleTabFocusMode',
+            'transpose', 'wordHighlighter', 'wordOperations', 'wordPartOperations']
+    };
+    config.plugins.unshift(new MonacoWebpackPlugin(options));
+    // Monaco ESM end
+    return config;
+};
