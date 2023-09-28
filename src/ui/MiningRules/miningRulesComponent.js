4fb04f014a74b767e34d0d78577405277104ecf3
saharmehrpour <smehrpou@masonlive.gmu.edu>
2023-09-28 11:43:37 -0400

    remove extra information from clustered rules panel

--- src/ui/MiningRules/miningRulesComponent.js
+++ src/ui/MiningRules/miningRulesComponent.js
@@ -267,18 +267,9 @@ class MiningRulesComponent extends Component {
         return (<div className={"minedRulesComponent"}>
             <div className={"generateRuleGui guiBoundingBox minedRuleBoundingBox"}>
                 <Row>
-                    <Col md={8}>
+                    <Col md={12}>
                         <MinedClusterRulePad key={new Date()} rulePadState={rulePadState}
                                              featureMetaData={this.state.featureMetaData} fileGroup={fileGroup}/>
-                    </Col>
-                    <Col md={3}>
-                        <h5><strong>Sum of feature weights in the cluster: </strong>{clusterObject.sumWeights}</h5>
-                        <h5><strong>Average utility of itemSets in the cluster: </strong>
-                            {clusterObject.cluster.reduce((sum, itemSet) => sum + itemSet.utility, 0)}</h5>
-                        <h5><strong>Size of the cluster: </strong>{clusterObject.cluster.length}</h5>
-                        <h5><strong>Average frequency (Support) of cluster members: </strong>
-                            {clusterObject.averageSupport}
-                        </h5>
                     </Col>
                 </Row>
             </div>
