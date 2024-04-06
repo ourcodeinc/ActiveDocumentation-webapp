42829c77252cafb60b0a50b451ea619889ef2fb4
Hayden Hanson <hansonhayd3n@gmail.com>
2024-04-06 13:39:52 -0400

    fix: xml to java parser working great now, ui fixes

--- src/activeLLM/suggestFix.js
+++ src/activeLLM/suggestFix.js
@@ -3,41 +3,36 @@ import OpenAI from "openai";
 
 // creates a fix for a code snippet that violates a certain design rule
 export async function suggestFix(rule, snippet, setState) {
-
-    console.log(snippet)
-
-    const prompt = `Here is a design rule and its description. ${rule} Now, 
+  const prompt = `Here is a design rule and its description. ${rule} Now, 
     here is a code snippet that violates this design rule. ${snippet} Suggest 
     a fix to make this violation follow the given design rule? Generate code, 
     with the surrounding code included, to make this violation follow the given 
     design rule. Be sure to maintain proper whitespace with \\t and \\n. 
-    Give a brief explanation of your fix as well. Strictly output in JSON format. 
-    The JSON should have the following format:{"code": "...", "explanation": "..."}`
+    Give a brief explanation of your fix as well. Strictly output in JSON format. The JSON should have the following format:{"code": "...", "explanation": "..."}`;
 
-    try {
-        const openai = new OpenAI({
-            // NOTE: open config.js and add your api key there 
-            apiKey: config.OPENAI_API_KEY,
-            // FIXME: no .env, need to allow browser`
-            dangerouslyAllowBrowser: true,
-        });
+  try {
+    const openai = new OpenAI({
+      // NOTE: open config.js and add your api key there
+      apiKey: config.OPENAI_API_KEY,
+      // FIXME: no .env, need to allow browser`
+      dangerouslyAllowBrowser: true,
+    });
 
-        const chatCompletion = await openai.chat.completions.create({
-            model: "gpt-3.5-turbo",
-            messages: [{ role: "user", content: prompt }],
-        });
+    const chatCompletion = await openai.chat.completions.create({
+      model: "gpt-3.5-turbo",
+      temperature: 0.75,
+      messages: [{ role: "user", content: prompt }],
+    });
 
-        const suggestedSnippet = chatCompletion.choices[0].message.content;
-        const stripped = suggestedSnippet.replace(/^`json|`json$/g, "").trim();
-        const parsedJSON = JSON.parse(stripped);
+    const suggestedSnippet = chatCompletion.choices[0].message.content;
+    const stripped = suggestedSnippet.replace(/^`json|`json$/g, "").trim();
+    const parsedJSON = JSON.parse(stripped);
 
-        // sets the relevant state in the React component that made the request
-        // see ../ui/rulePanel.js for more details
-        setState({ suggestedSnippet: parsedJSON["code"] });
-        setState({ snippetExplanation: parsedJSON["explanation"] });
-
-    } catch (error) {
-        console.log(error);
-    }
-};
-
+    // sets the relevant state in the React component that made the request
+    // see ../ui/rulePanel.js for more details
+    setState({ suggestedSnippet: parsedJSON["code"] });
+    setState({ snippetExplanation: parsedJSON["explanation"] });
+  } catch (error) {
+    console.log(error);
+  }
+}
